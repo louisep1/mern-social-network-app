7defd51ca30e634e379ed7961a8c4db2f59e6b9e
louisep1 <89385533+louisep1@users.noreply.github.com>
2023-01-22 18:08:03 +0900

    removed socketio

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const express = require('express')
-
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./backend/middleware/errorMiddleware')
